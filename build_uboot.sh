e9c13950b67e65431221e1a6d6935cf01d95006a
Mykhailo Sopiha <mykhailo.sopiha@linaro.org>
2018-10-25 18:39:20 +0300

    Updated u-boot script
    Signed-off-by: Mykhailo Sopiha <mykhailo.sopiha@linaro.org>

--- build_uboot.sh
+++ build_uboot.sh
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
-source ~/scripts/config.sh $1
+source ~/scripts/config.sh $*
 cd $UBOOTDIR
-make am57xx_evm_defconfig
+make $(value $TARGET[ubootconfig])
 make -j4 || error "Build failed"
 prnt_green "Congratulations! Your u-boot build was successful!"
