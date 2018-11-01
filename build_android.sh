4376af03c491c0e6ffae326a10145c908af4016f
Mykhailo Sopiha <mykhailo.sopiha@linaro.org>
2018-11-01 16:12:45 +0200

    -Add m_x15 target -Add prebuilt kernel -Add kernel images copy request from current baseline if kernel is  supposed to be prebuilt
    Signed-off-by: Mykhailo Sopiha <mykhailo.sopiha@linaro.org>

--- build_android.sh
+++ build_android.sh
@@ -2,7 +2,26 @@
 
 source ~/scripts/config.sh $*
 
+ZIMAGE=$KERNELDIR/arch/arm/boot/zImage
+DTB=$KERNELDIR/arch/arm/boot/dts/ti/am57xx-beagle-x15-revc.dtb
+
+if [ $(value $TARGET[prebuilt_kernel]) == true ]; then
+    [[ -f $ZIMAGE ]] || error "No zImage found -> $ZIMAGE"
+    [[ -f $DTB ]] || error "No dtb found -> $DTB"
+    echo ""
+    read -r -p "You are going to pull kernel images from $KERNELDIR to replace prebuilt. Are you sure [y/N] " response
+    response=${response,,}    # tolower
+    if [[ "$response" =~ ^(yes|y)$ ]]; then
+        cp $ZIMAGE $AOSPDIR/$(value $TARGET[aosp_kernel_path])
+        cp $DTB $AOSPDIR/$(value $TARGET[aosp_kernel_path])
+        echo "Copied..."
+    else
+        echo "Already existing kernel images from $(value $TARGET[aosp_kernel_path]) will be used\n"
+    fi
+fi
+
 cd $AOSPDIR
+
 source build/envsetup.sh && lunch $(value $TARGET[lunch]) || error "Something went wrong. check AOSPDIR"
 make -j4 || error "Android build failed"
 prnt_green "Congratulations! Your Androud build was successful for target $(value $TARGET[target]) in $AOSPDIR!"
---
+++ copy_and_upload.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+~/scripts/copy.sh $* && ~/scripts/upload.sh $*
